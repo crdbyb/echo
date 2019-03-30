13d34dd836dc10ae9088fdcc5e5db3514ae2a8a9
crdbyb <crdbyb@example.com>
2019-03-30 20:15:06 +0800

    .

---
+++ string/test.cpp
@@ -0,0 +1,19 @@
+#include "mystring.h"
+#include "string.cpp"
+#include <iostream>
+using namespace std;
+int main(){
+	Mystring s1(" sss");
+	Mystring s2(" www");
+	Mystring s3 = s1;
+	Mystring s4 = s1 + s2;
+	cout << s3;
+	cout << s4;
+	if(s1 > s2){
+		cout << "1" << endl;
+	}
+	else{
+		cout << "2" << endl;
+	}
+	cout << s2.size() << endl;
+}
